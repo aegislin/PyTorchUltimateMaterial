dd5a23dff3d0a3b845a8eb1a70e10232aa6b8f1a
aegis <aegislin@gmail.com>
2024-04-25 00:25:14 +0900

    Training loop logic fixed, and use GPU if possible. Restoring num_epochs to 2000.

--- 220_GAN/Gan_end.py
+++ 220_GAN/Gan_end.py
@@ -10,6 +10,13 @@ from random import uniform
 
 import seaborn as sns
 sns.set(rc={'figure.figsize':(12,12)})
+
+# %% cuda test
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+if device == 'cpu':
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+print(f'device={device}')
+
 #%% create training data
 TRAIN_DATA_COUNT = 1024
 theta = np.array([uniform(0, 2 * np.pi) for _ in range(TRAIN_DATA_COUNT)]) # np.linspace(0, 2 * np.pi, 100)
@@ -44,7 +51,7 @@ discriminator = nn.Sequential(
             nn.Dropout(0.3),
             nn.Linear(64, 1),
             nn.Sigmoid(),
-        )
+        ).to(device)
 
 generator = nn.Sequential(
             nn.Linear(2, 16),
@@ -52,23 +59,26 @@ generator = nn.Sequential(
             nn.Linear(16, 32),
             nn.ReLU(),
             nn.Linear(32, 2),
-        )
+        ).to(device)
 
 # %% training
 LR = 0.001
-NUM_EPOCHS = 2
+NUM_EPOCHS = 2000
 loss_function = nn.BCELoss()
 optimizer_discriminator = torch.optim.Adam(discriminator.parameters())
 optimizer_generator = torch.optim.Adam(generator.parameters())
 
+loss_generator = None
+loss_discriminator = None
+
 for epoch in range(NUM_EPOCHS):
     for n, (real_samples, _) in enumerate(train_loader):
         # Data for training the discriminator
-        real_samples_labels = torch.ones((BATCH_SIZE, 1))
-        latent_space_samples = torch.randn((BATCH_SIZE, 2))
+        real_samples_labels = torch.ones((BATCH_SIZE, 1), device=device)
+        latent_space_samples = torch.randn((BATCH_SIZE, 2), device=device)
         generated_samples = generator(latent_space_samples)
-        generated_samples_labels = torch.zeros((BATCH_SIZE, 1))
-        all_samples = torch.cat((real_samples, generated_samples))
+        generated_samples_labels = torch.zeros((BATCH_SIZE, 1), device=device)
+        all_samples = torch.cat((real_samples.to(device), generated_samples))
         all_samples_labels = torch.cat(
             (real_samples_labels, generated_samples_labels)
         )
@@ -84,7 +94,7 @@ for epoch in range(NUM_EPOCHS):
 
         if epoch % 2 == 1:
             # Data for training the generator
-            latent_space_samples = torch.randn((BATCH_SIZE, 2))
+            latent_space_samples = torch.randn((BATCH_SIZE, 2), device = device)
 
             # Training the generator
             generator.zero_grad()
@@ -96,28 +106,30 @@ for epoch in range(NUM_EPOCHS):
             loss_generator.backward()
             optimizer_generator.step()
     
-            # Show progress
-            if epoch % 10 == 0:
-                print(epoch)
-                print(f"Epoch {epoch}, Discriminator Loss {loss_discriminator}")
-                print(f"Epoch {epoch}, Generator Loss {loss_generator}")
-                with torch.no_grad():
-                    latent_space_samples = torch.randn(1000, 2)
-                    generated_samples = generator(latent_space_samples).detach()
-                plt.figure()
-                plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
-                plt.xlim((-20, 20))
-                plt.ylim((-20, 15))
-                plt.text(10, 15, f"Epoch {epoch}")
-                plt.savefig(f"train_progress/image{str(epoch).zfill(3)}.jpg")
+    # Show progress
+    if epoch % 10 == 0:
+        print(epoch)
+        print(f"Epoch {epoch}, Discriminator Loss {loss_discriminator}")
+        print(f"Epoch {epoch}, Generator Loss {loss_generator}")
+        with torch.no_grad():
+            latent_space_samples = torch.randn(1000, 2, device=device)
+            generated_samples = generator(latent_space_samples).detach()
+        """            
+        plt.figure()
+        plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
+        plt.xlim((-20, 20))
+        plt.ylim((-20, 15))
+        plt.text(10, 15, f"Epoch {epoch}")
+        plt.savefig(f"train_progress/image{str(epoch).zfill(3)}.jpg")
+        """
 
 
     
 
 # %% check the result
-latent_space_samples = torch.randn(10000, 2)
+latent_space_samples = torch.randn(10000, 2, device=device)
 generated_samples = generator(latent_space_samples)
-generated_samples = generated_samples.detach()
+generated_samples = generated_samples.detach().cpu()
 plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
 plt.text(10, 15, f"Epoch {epoch}")
 
